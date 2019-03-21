970e8e87741001592ee48cb9af87c28467574a6f
markhartnett <mark.hartnett@ucdconnect.ie>
2019-03-21 13:42:42 +0000

    Method detection now functioning on a number of different tested files, still will need to be fine tuned

--- src/files/MethodDetector.java
+++ src/files/MethodDetector.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MethodDetector {
 
@@ -11,43 +12,43 @@ public class MethodDetector {
     public void checkForMethods(String directoryAddress) throws Exception {
         File dir = new File(directoryAddress);
 
-        //loops through each file in the directory
-        for (File fileDir : dir.listFiles()) {
+        for (File file : dir.listFiles()) {
             ArrayList<SLMethod> methods = new ArrayList<>();
-            if(fileDir.getName().substring(fileDir.getName().indexOf(".") + 1).equals("java")) {
-                BufferedReader br1 = new BufferedReader(new FileReader(fileDir));
-                BufferedReader br2 = new BufferedReader(new FileReader(fileDir));
 
-                String st1;
-                String st2 = br2.readLine();
+            if(file.getName().substring(file.getName().indexOf(".") + 1).equals("java")) {
+                BufferedReader br1 = new BufferedReader(new FileReader(file));
+                BufferedReader br2 = new BufferedReader(new FileReader(file));
 
-                while ((st1 = br1.readLine()) != null) {
-                    st2 = br2.readLine();
+                String firstLine;
+                String secondLine = br2.readLine();
+
+                while ((firstLine = br1.readLine()) != null) {
+                    secondLine = br2.readLine();
                     String name = "";
                     String accessor = "";
+                    String returnType = "";
+                    ArrayList<String> parameters = new ArrayList<String>();
+                    String[] firstLineSplit = firstLine.split(" ");
 
-                    if(isMethod(st1, st2)){
-                        String[] subParts = st1.split(" ");
-                        for(int i = 0; i < subParts.length; i++){
-                            if(subParts[i].contains("(") && !subParts[i].startsWith("(")){
-                                name = subParts[i].split("\\(")[0];
-                            }
-                            else if(subParts[i].startsWith("(")){
-                                name = subParts[i-1];
-                            }
-                            if(subParts[i].matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)")){
-                                accessor += " " + subParts[i];
-                            }
-                        }
-                        methods.add(new SLMethod(name,accessor));
+                    if(isMethod(firstLine, secondLine)){
+                        name = findMethodName(firstLineSplit);
+                        accessor = findAccessor(firstLineSplit);
+                        returnType = findReturnType(firstLineSplit,name);
+                        parameters = findParameters(firstLine);
+
+                        methods.add(new SLMethod(name, accessor, returnType, parameters));
+
                     }
-
+                    else if(isConstructor(firstLine,secondLine,file.getName())){
+                        name = findMethodName(firstLineSplit);
+                        parameters = findParameters(firstLine);
+                        methods.add(new SLMethod(name,accessor,returnType,parameters));
+                    }
                 }
 
-                System.out.println(fileDir.getName());
-                System.out.println(methods.size());
+                System.out.println(file.getName());
                 for (int i = 0; i < methods.size(); i++) {
-                    System.out.println(methods.get(i).getAccessor() + " " + methods.get(i).getName());
+                    System.out.println(methods.get(i).toString());
                 }
                 System.out.println();
             }
@@ -56,27 +57,112 @@ public class MethodDetector {
     }
 
     public static boolean isMethod(String firstLine, String secondLine){
-        String st1Copy = firstLine.replaceAll(" ", "");
+        String firstLineNoSpaces = firstLine.replaceAll(" ", "");
+        firstLineNoSpaces = firstLineNoSpaces.replaceAll("\t","");
         String st2Copy = "";
         if(secondLine!=null) {
             st2Copy = secondLine.replaceAll(" ", "");
+            st2Copy = st2Copy.replaceAll("\t","");
         }
 
-        if(st1Copy.matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)+([a-zA-Z]+)([a-zA-Z]+)\\(.*\\)\\{")){
-            System.out.println(firstLine);
+        if(firstLineNoSpaces.matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)+(.+)\\(.*\\).*\\{")){
             return true;
         }
-        if(firstLine.matches("\\s*([a-zA-Z]+)\\s+([a-zA-Z]+)\\s*\\(.*\\)\\s*\\{\\s*") && !firstLine.contains("else if")){
-            System.out.println(firstLine);
+        else if(firstLine.matches("\\s*([a-zA-Z]+)\\s+([a-zA-Z]+)\\s*\\(.*\\)\\s*.*\\s*\\{\\s*") && !firstLine.contains("else if")){
             return true;
         }
-        if(st1Copy.matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)+([a-zA-Z]+)([a-zA-Z]+)\\(.*\\)") && st2Copy.startsWith("{")){
+        else if(firstLineNoSpaces.matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)+([a-zA-Z]+)([a-zA-Z]+)\\(.*\\).*") && st2Copy.startsWith("{")){
             return true;
         }
-        if(firstLine.matches("\\s*([a-zA-Z]+)\\s+([a-zA-Z]+)\\s*\\(.*\\)\\s*") && !firstLine.contains("else if") && st2Copy.startsWith("{")){
+        else if(firstLine.matches("\\s*([a-zA-Z]+)\\s+([a-zA-Z]+)\\s*\\(.*\\)\\s*.*\\s*") && !firstLine.contains("else if") && st2Copy.startsWith("{")){
             return true;
         }
-        return false;
+        else {
+            return false;
+        }
     }
 
+    public static boolean isConstructor(String firstLine, String secondLine, String fileName){
+        String firstLineNoSpaces = firstLine.replaceAll(" ", "");
+        firstLineNoSpaces = firstLineNoSpaces.replaceAll("\t","");
+        String st2Copy = "";
+
+        if(secondLine!=null) {
+            st2Copy = secondLine.replaceAll(" ", "");
+            st2Copy = st2Copy.replaceAll("\t","");
+        }
+
+        String className = fileName.split("\\.")[0];
+
+        if(firstLineNoSpaces.matches("([a-zA-Z]+)\\(.*\\)\\{") && firstLineNoSpaces.contains(className)){
+            return true;
+        }
+        else if(firstLineNoSpaces.matches("([a-zA-Z]+)\\(.*\\)") && firstLineNoSpaces.contains(className) && st2Copy.startsWith("{")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static String findMethodName(String[] firstLineSplit){
+        String name = "";
+        for(int i = 0; i < firstLineSplit.length; i++) {
+            if (firstLineSplit[i].contains("(") && !firstLineSplit[i].startsWith("(")) {
+                name = firstLineSplit[i].split("\\(")[0];
+            } else if (firstLineSplit[i].startsWith("(")) {
+                name = firstLineSplit[i - 1];
+            }
+        }
+        return name;
+    }
+
+    public static String findAccessor(String[] firstLineSplit){
+        String accessor = "";
+        for(int i = 0; i < firstLineSplit.length; i++) {
+            if (firstLineSplit[i].matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)")) {
+                accessor += " " + firstLineSplit[i];
+            }
+        }
+        return accessor;
+    }
+
+    public static String findReturnType(String[] firstLineSplit, String name){
+        String returnType = "";
+        for(int i = 0; i<firstLineSplit.length; i++) {
+            if (firstLineSplit[i].matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)") && !firstLineSplit[i + 1].matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)") && !firstLineSplit[i + 1].contains(name)) {
+                returnType = firstLineSplit[i + 1];
+            }
+        }
+        //if no return type has been found it means there is no accessor and therefor
+        //the return type will be the first word in the declaration or there is no return type
+        if(returnType.equals("")){
+            for (String r: firstLineSplit){
+                if(!r.equals("") && !r.matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)")){
+                    returnType = r;
+                    break;
+                }
+                else if(!r.equals("") && r.matches("(public|private|protected|static|final|native|synchronized|abstract|threadsafe|transient)")){
+                    break;
+                }
+            }
+        }
+        return returnType;
+    }
+
+    public static ArrayList<String> findParameters(String firstline){
+        ArrayList<String> parameters = new ArrayList<String>();
+        String allParams = "";
+        String[] splitAtBracket = firstline.split("\\(");
+
+        for (String x:splitAtBracket) {
+            if(x.contains(")") && x.split("\\)").length>0){
+                allParams = x.split("\\)")[0];
+            }
+        }
+
+        parameters.addAll(Arrays.asList(allParams.split(",")));
+
+        return parameters;
+    }
 }
--- src/files/SLMethod.java
+++ src/files/SLMethod.java
@@ -1,18 +1,17 @@
 package files;
-
 import java.util.ArrayList;
 
 public class SLMethod {
     private String accessor;
     private String name;
     private String returnType;
-    private ArrayList parameters;
+    private ArrayList<String> parameters;
 
-    public SLMethod(String name, String accessor) {
+    public SLMethod(String name, String accessor, String returnType, ArrayList parameters) {
         this.name = name;
         this.accessor = accessor;
-        //this.returnType = returnType;
-        //this.parameters = parameters;
+        this.returnType = returnType;
+        this.parameters = parameters;
     }
 
     public String getAccessor(){
@@ -23,11 +22,26 @@ public class SLMethod {
         return name;
     }
 
-    /*public String getReturnType() {
+    public String getReturnType() {
         return returnType;
-    }*/
+    }
 
-    /*public ArrayList getParameters() {
+    public ArrayList getParameters() {
         return parameters;
-    }*/
+    }
+
+    @Override
+    public String toString() {
+        String result = accessor + " " + returnType + " " + name + "(" ;
+        for (int i = 0; i < parameters.size(); i++) {
+            if(i<parameters.size()-1) {
+                result += parameters.get(i) + ",";
+            }
+            else {
+                result += parameters.get(i);
+            }
+        }
+        result += ")";
+        return result;
+    }
 }
