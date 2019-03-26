02e447e92bfadebc89ea771365d15b814ca657f6
ConorMeegan <conmeegan@gmail.com>
2019-03-26 15:40:21 +0000

    Regex done that recognises all declared variables in a file

--- src/files/VariableDetector.java
+++ src/files/VariableDetector.java
@@ -1,33 +1,97 @@
 package files;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class VariableDetector {
 
-    public ArrayList<SLVariable> getVariables(Class<?> myClass) {
+    String dataTypeRegex = String.format("(byte|short|int|long|float|double|boolean|char|String)");
+    String accessorsRegex = String.format("(public|private|protected|static|final|native|synchronized|abstract|transient)");
 
-        Field[] classFields = myClass.getDeclaredFields();
-        Method[] classMethods = myClass.getDeclaredMethods();
-        ArrayList<SLVariable> variables = new ArrayList<>();
+    public void checkForVariables(String directoryAddress) throws Exception {
+        File dir = new File(directoryAddress);
 
-        //used to get constructor variables
-        for (Field f:classFields) {
-            System.out.println(f.getType()+ " "+f.getName());
+        for (File fileDir : dir.listFiles()) {
+            ArrayList<SLVariable> variables = new ArrayList<>();
+
+            if(fileDir.getName().substring(fileDir.getName().indexOf(".") + 1).equals("java")) {
+                BufferedReader br = new BufferedReader(new FileReader(fileDir));
+
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    String variableScope = "";
+                    String variableType = "";
+                    String variableName = "";
+                    String variableValue = "";
+                    String[] lineSplit =line.split("\\s+");
+
+                    if (isVariable(line)){
+                        variableScope = findScope(lineSplit);
+                        variableType = findType(lineSplit);
+                        variableName = findName(lineSplit);
+                        variableValue = findValue(lineSplit);
+                    }
+
+                }
+            }
+
         }
+    }
 
-        //used to get the variables passed in to a method
-        for (Method m:classMethods) {
-            for (Parameter p:m.getParameters()) {
-                //first variable is called arg0, arg1, etc.
-                System.out.println(p.getType()+": "+p.getName());
+    public boolean isVariable(String line) {
+
+        line = line.replaceAll("\\s+", "");
+
+        if (line.matches(dataTypeRegex+".*")) {
+            if (line.contains("=")||line.contains(";")) {
+                return true;
+            } else {
+                return false; //is a poorly declared method without the scope defined (smelly!!!)
+            }
+
+        } else if (line.matches(".*=new.*")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String findScope(String[] lineSplit) {
+        String scope = "";
+
+        for(int i=0; i < lineSplit.length; i++) {
+            if (lineSplit[i].matches(accessorsRegex)) {
+                scope += " " + lineSplit[i];
             }
         }
 
+        return scope;
+    }
 
+    public String findType(String[] lineSplit) {
+        String type = "";
 
-        return null;
+        for (int i=0; i<lineSplit.length; i++) {
+            if (lineSplit[i].matches(dataTypeRegex)) {
+                type = lineSplit[i];
+            }
+        }
+
+        return type;
+    }
+
+    public String findName(String[] lineSplit) {
+        String name = "";
+
+        return name;
+    }
+
+    public String findValue(String[] lineSplit) {
+        String value = "";
+
+        return value;
     }
 }
