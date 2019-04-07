26d8b4fc0190535a109099f8d542999f540a6de5
markhartnett <mark.hartnett@ucdconnect.ie>
2019-04-07 20:32:03 +0100

    god class implementation general idea, will need to be changed up and tested

--- src/files/SLClass.java
+++ src/files/SLClass.java
@@ -4,9 +4,42 @@ import java.util.ArrayList;
 
 public class SLClass {
 
-    ArrayList<SLMethod> methodList = new ArrayList<>();
-    ArrayList<SLVariable> variableList = new ArrayList<>();
-    ArrayList<SLClass> subClassList = new ArrayList<>();
+    String name = "";
+    ArrayList<SLMethod> methods = new ArrayList<>();
+    ArrayList<SLVariable> variables = new ArrayList<>();
+    ArrayList<SLClass> subClasses = new ArrayList<>();
     SLClass parentClass = null;
+
+    public SLClass(String name, ArrayList<SLMethod> methods, ArrayList<SLVariable> variables, ArrayList<SLClass> subClasses, SLClass parentClass){
+        this.name = name;
+        this.methods = methods;
+        this.variables = variables;
+        this.subClasses = subClasses;
+        this.parentClass = parentClass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<SLMethod> getMethods() {
+        return methods;
+    }
+
+    public ArrayList<SLVariable> getVariables() {
+        return variables;
+    }
+
+    public ArrayList<SLClass> getSubClasses() {
+        return subClasses;
+    }
+
+    public SLClass getParentClass() {
+        return parentClass;
+    }
+
+    public boolean hasParentClass(){
+        return parentClass != null;
+    }
 
 }
---
+++ src/smells/GodClass.java
@@ -0,0 +1,50 @@
+package smells;
+
+import files.*;
+
+/*
+A God Class is a class which either knows or does too much
+To see if a class is a god class or not, a number of metrics will be needed to detect
+this. We will try to detect this by seeing how much access a class has to foreign data,
+how much the object does that doesn't relate to itself, the number of lines and the number
+of methods in the class
+ */
+public class GodClass {
+    public boolean isGodClass(SLClass cls) {
+        if((isLoosleyCoupled(cls)||hasLowCohesion(cls)) && cls.getMethods().size()>20){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    private boolean isLoosleyCoupled(SLClass cls){
+        int numOfNonPrimitive = 0;
+
+        for (SLVariable variable : cls.getVariables()) {
+            if(!variable.getType().matches("(byte|short|int|long|float|double|boolean|char|String)")){
+                numOfNonPrimitive ++;
+            }
+        }
+
+        return numOfNonPrimitive>20;
+    }
+
+    private boolean hasLowCohesion(SLClass cls){
+        int numPrivateMethods = 0;
+        int numPublicMethods = 0;
+
+        for (SLMethod method : cls.getMethods()) {
+            if(method.getAccessor().contains("public")){
+                numPublicMethods++;
+            }
+            else if(method.getAccessor().contains("public")){
+                numPrivateMethods++;
+            }
+        }
+
+        return numPrivateMethods>numPublicMethods;
+    }
+}
